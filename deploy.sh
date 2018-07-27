e914a114011c9ca412aef7730928bcd2bb3c2986
Jacob Tomlinson <jacob.tomlinson@informaticslab.co.uk>
2018-07-27 09:02:18 +0100

    Update deploy with more intelligent tagging

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,18 @@ set -e
 # Login
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-# Add new tags
-docker tag informaticslab/pangeo-notebook:$TRAVIS_COMMIT informaticslab/pangeo-notebook:latest
+# If this isn't a tagged commit push the hash and update dev
+# If this is a tagged commit push that tag and update latest
+if [ "$TRAVIS_TAG" = "" ]; then
+    # Push the image to Docker Hub
+    docker push informaticslab/pangeo-notebook:${TRAVIS_COMMIT:0:8}
 
-# Push to Docker Hub
-docker push informaticslab/pangeo-notebook:$TRAVIS_COMMIT
-docker push informaticslab/pangeo-notebook:latest
+    # Replace the dev tag with the latest commit
+    docker tag informaticslab/pangeo-notebook:${TRAVIS_COMMIT:0:8} informaticslab/pangeo-notebook:dev
+    docker push informaticslab/pangeo-notebook:dev
+else
+    docker tag informaticslab/pangeo-notebook:${TRAVIS_COMMIT:0:8} informaticslab/pangeo-notebook:$TRAVIS_TAG
+    docker push informaticslab/pangeo-notebook:$TRAVIS_TAG
+    docker tag informaticslab/pangeo-notebook:${TRAVIS_COMMIT:0:8} informaticslab/pangeo-notebook:latest
+    docker push informaticslab/pangeo-notebook:latest
+fi
