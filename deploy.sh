b1dc42fba06c1a09b68f24542a1633c8016ce824
Jacob Tomlinson <jacob.tomlinson@informaticslab.co.uk>
2018-10-30 11:03:54 +0000

    Add AWS ECR registry

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,29 @@
 
 set -e
 
-# Login
+# Login to docker hub
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-# If this isn't a tagged commit push the hash and update dev
-# If this is a tagged commit push that tag and update latest
-if [ "$TRAVIS_TAG" = "" ]; then
-    # Push the image to Docker Hub
-    docker push informaticslab/pangeo-notebook:${TRAVIS_COMMIT:0:8}
+# Lockin to AWS ECR (Requires AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env vars to be set)
+$(aws ecr get-login --no-include-email --region eu-west-2)
 
-    # Replace the dev tag with the latest commit
-    docker tag informaticslab/pangeo-notebook:${TRAVIS_COMMIT:0:8} informaticslab/pangeo-notebook:dev
-    docker push informaticslab/pangeo-notebook:dev
-else
-    docker tag informaticslab/pangeo-notebook:${TRAVIS_COMMIT:0:8} informaticslab/pangeo-notebook:$TRAVIS_TAG
-    docker push informaticslab/pangeo-notebook:$TRAVIS_TAG
-    docker tag informaticslab/pangeo-notebook:${TRAVIS_COMMIT:0:8} informaticslab/pangeo-notebook:latest
-    docker push informaticslab/pangeo-notebook:latest
-fi
+for REPOSITORY in 'informaticslab/pangeo-notebook' '536099501702.dkr.ecr.eu-west-2.amazonaws.com/pangeo-notebook'; do
+
+    # If this isn't a tagged commit push the hash and update dev
+    # If this is a tagged commit push that tag and update latest
+    if [ "$TRAVIS_TAG" = "" ]; then
+        # Push the image to Docker Hub
+        docker tag pangeo-notebook:${TRAVIS_COMMIT:0:8} REPOSITORY:${TRAVIS_COMMIT:0:8}
+        docker push ${REPOSITORY}:${TRAVIS_COMMIT:0:8}
+
+        # Replace the dev tag with the latest commit
+        docker tag ${REPOSITORY}:${TRAVIS_COMMIT:0:8} ${REPOSITORY}:dev
+        docker push ${REPOSITORY}:dev
+    else
+        docker tag ${REPOSITORY}:${TRAVIS_COMMIT:0:8} ${REPOSITORY}:$TRAVIS_TAG
+        docker push ${REPOSITORY}:$TRAVIS_TAG
+        docker tag ${REPOSITORY}:${TRAVIS_COMMIT:0:8} ${REPOSITORY}:latest
+        docker push ${REPOSITORY}:latest
+    fi
+
+done
